ce02a6140daf164c980b0a1d6157b7238461867b
Farrukh <fa55ukh@gmail.com>
2019-09-26 20:26:23 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Player from "./components/Player";
+
+class App extends React.Component {
+  state = {
+    players: [
+      {
+        name: "Real Madrid",
+        id: 1
+      },
+      {
+        name: "Barcelona",
+        id: 2
+      },
+      {
+        name: "Bayern",
+        id: 3
+      },
+      {
+        name: "Liverpool",
+        id: 4
+      }
+    ]
+  };
+
+  handleRemovePlayer = id => {
+    this.setState(prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id)
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header
+          title="Game Scoreboard"
+          totalPlayers={this.state.players.length}
+        />
+
+        {/* Players list */}
+        {this.state.players.map(player => (
+          <Player
+            name={player.name}
+            id={player.id}
+            key={player.id.toString()}
+            remove={this.handleRemovePlayer}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
